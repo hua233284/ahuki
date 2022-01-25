f0c649f8d4b927e7d11343caa6af7fae3a83c826
hua233284 <98403544+hua233284@users.noreply.github.com>
2022-01-25 23:32:42 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 <html>
 
 <head>
-    <title>吃掉小鹿乃</title>
-    <meta itemprop="name" content="吃掉小鹿乃" />
+    <title>吃掉群主</title>
+    <meta itemprop="name" content="吃掉群主" />
     <meta itemprop="description" content="新概念音游" />
     <meta charset="utf-8" />
     <meta name="viewport" content="initial-scale=1, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0, width=device-width,target-densitydpi=device-dpi" />
